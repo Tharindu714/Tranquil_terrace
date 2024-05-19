87fe8b3974ebd5cbc79243f54183c8d5b2e3eacb
Tharindu714 <110843121+Tharindu714@users.noreply.github.com>
2024-05-19 22:00:18 +0530

    Admin Sign in Save Button removed

--- src/GUI/Splash.java
+++ src/GUI/Splash.java
@@ -1,28 +1,52 @@
 package GUI;
 
 import com.formdev.flatlaf.IntelliJTheme;
+import java.awt.Color;
 
 public class Splash extends javax.swing.JFrame {
 
     public Splash() {
         initComponents();
+        setLocationRelativeTo(null);
+        setBackground(new Color(0, 0, 0, 0));
+        Thread th = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        jProgressBar1.setValue(i);
+                        try {
+                            Thread.sleep(25);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Dashboard d = new Dashboard();
+                    d.setVisible(true);
+                    this.dispose();
+                }
+        );
+        th.start();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jProgressBar1 = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setEnabled(false);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jProgressBar1.setForeground(new java.awt.Color(245, 71, 104));
+        getContentPane().add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 360, 410, 20));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/WELCOME.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 450));
-        getContentPane().add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 360, 410, 20));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
